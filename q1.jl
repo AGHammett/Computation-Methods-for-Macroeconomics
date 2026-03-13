faefc3083cfb9c2cfb39c23459b55587aa11e5fc
Alex <laseralex1@gmail.com>
2026-03-13 17:29:44 +0000

    q1 file

---
+++ q1.jl
@@ -0,0 +1,30 @@
+using Plots
+using Optim
+
+u(c::Float64, gamma:: Real)::Float64 = (c^(1 - gamma)) / (1 - gamma)
+
+v(h::Float64, sigma:: Real)::Float64 = ((1 - h)^(1 - 1 / sigma)) / (1 - 1 / sigma)
+
+function Z(h::Float64 ; w::Real = 2, gamma:: Real = 3, sigma:: Real = 0.5, a:: Real = 0.5):: Float64
+
+    c = w * h + a
+
+    return u(c, gamma) + v(h, sigma)
+
+end
+
+function plot_function(f:: Function; p_range:: Tuple{Real, Real} = (0, 0.9), grid_points:: Int = 100)
+
+    grid = range(p_range[1], p_range[2], grid_points)
+    
+    p = plot(grid, f.(grid), label = "Function")
+    display(p)
+    return p
+
+end
+
+function optimise_Z()
+
+    
+
+end
